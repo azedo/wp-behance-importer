9f9bebfb2ae30da9cc6944f2e60c8b72759d8e35
Eduardo Grigolo <eduardo.grigolo@gmail.com>
2016-03-31 11:50:27 +0100

    Comment the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 var gulp = require('gulp');
 
 // Include other plugins
-var stylus = require('gulp-stylus');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
+var stylus = require('gulp-stylus'); // Convert stylus to css
+var concat = require('gulp-concat'); // Concatenate the js files
+var uglify = require('gulp-uglify'); // Minify the js files
+var rename = require('gulp-rename'); // Rename the files
+var autoprefixer = require('gulp-autoprefixer'); // Autoprefix the css file
+var jshint = require('gulp-jshint'); // Search the main js file for errors
+var stylish = require('jshint-stylish'); // Display the errors in a better way
 
 // Lint Task
 gulp.task('lint', function() {
